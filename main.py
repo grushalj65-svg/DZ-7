621c2123ed811872f51561d492e9792e312f8bd6
grushalj65-svg <grushalj65@gmail.com>
2025-11-21 21:08:23 +0200

    2

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-class Words:
-    def __init__(self, data):
-        self.data = data
+class Pow2:
+    def __init__(self, stop):         #Максимальний показник степеня.
+        self.stop = stop
 
-    def show(self):         #виводимо довжини всіх слів.
-        i = 0
-        while i < self.count_items():
-            w = self.data[i]              #Перебирає кожен символ слова.
-            c = 0
-            for _ in w:
-                c = c + 1
-            print(c)
-            i = i + 1
-
-    def count_items(self):
-        k = 0
-        for _ in self.data:
-            k = k + 1
-        return k
+    def show(self):
+        p = 0
+        v = 1
+        while p <= self.stop:          #Працює, поки степінь не досягне stop.
+            print(v)
+            v = v * 2
+            p = p + 1
 
 
-Words(["apple", "banana", "kiwi"]).show()
-
+Pow2(5).show()
